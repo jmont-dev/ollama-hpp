ce65e695194e94a58eff8be90c643733c117266a
James Montgomery <James.Brian.Montgomery@gmail.com>
2024-04-28 10:00:15 -0400

    Updates for basic generation and streaming endpoints.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
 
 #include <iostream>
 #include <string>
+#include <functional>
+
+
+void on_receive_token(const std::string& token)
+{
+
+    //std::istringstream iss(res->body);
+
+    //std::string line;
+    //while(std::getline(iss,line))
+    //{            
+        json chunk = json::parse(token);        
+        std::string response = chunk["response"];
+        std::cout << response << std::endl;
+    //}
+
+}
 
 int main()
 {
-    Ollama ollama;
-    std::cout << ollama.generate("mistral", "Why is the sky blue?") << std::endl;
+    //Ollama ollama;
+    std::cout << ollama::generate("llama3", "Why is the sky blue?") << std::endl;
+
+    //std::cout << ollama.generate("mistral", "Why is the sky blue?", std::bind(on_receive_token)) << std::endl;
+
 }
--- ollama.hpp
+++ ollama.hpp
@@ -9,14 +9,12 @@
 
 #include <string>
 #include <iostream>
+#include <functional>
 
 using json = nlohmann::json;
 
-
-
 class Ollama
 {
-
     public:
 
         Ollama(const std::string& url)
@@ -26,10 +24,10 @@ class Ollama
         }
 
         Ollama(): Ollama("http://localhost:11434") {}
-
         ~Ollama() {}
 
-    std::string generate(const std::string& model,const std::string& prompt)
+    // Generate a non-streaming reply as a string.
+    std::string generate(const std::string& model,const std::string& prompt, bool return_as_json=false)
     {
         std::string response="";
 
@@ -37,26 +35,60 @@ class Ollama
         json request;
         request["model"] = model;
         request["prompt"] = prompt;
+        request["stream"] = false;
+
+        std::string request_string = request.dump();
+
+        //std::cout << request_string << std::endl;      
+
+        if (auto res = this->cli->Post("/api/generate",request_string, "application/json"))
+        {
+
+            //std::istringstream iss(res->body);
+
+            //std::string line;
+            //while(std::getline(iss,line))
+            //{
+                if (return_as_json)
+                    response+=res->body;
+                else
+                {
+                    json chunk = json::parse(res->body);        
+                    response+=chunk["response"];
+                }
+            //}
+        }
+
+        return response;
+    }
+
+    // Generate a streaming reply where a user-defined callback function is invoked when each token is received.
+    void generate(const std::string& model,const std::string& prompt, std::function<void(const std::string&)> on_receive_token)//, bool receive_json=false)
+    {
+        std::string response="";
+
+        // Generate the JSON request
+        json request;
+        request["model"] = model;
+        request["prompt"] = prompt;
+        request["stream"] = true;
 
         std::string request_string = request.dump();
 
         std::cout << request_string << std::endl;
 
-        auto res = this->cli->Post("/api/generate",request_string, "application/json");
+        auto stream_callback = [on_receive_token](const char *data, size_t data_length) {std::string token(data, data_length); on_receive_token(token);};
 
-        if (res)
-        {
-            std::istringstream iss(res->body);
+        httplib::Headers headers = { {"Content-Type","application/json"}};
 
-            std::string line;
-            while(std::getline(iss,line))
-            {            
-                json chunk = json::parse(line);        
-                response+=chunk["response"];
-            }
-        }
+        //this->cli->Post("/api/generate", headers, request_string, "application/json",[&](const char* data, size_t data_length) {return true;})
 
-        return response;
+        //if (auto res =  ) //stream_callback))
+        //{
+        //   if (res->status==200)
+        //        throw new std::runtime_error("Received HTTP error: "+res.error());
+        //}
+
     }
 
     bool is_running()
@@ -100,6 +132,13 @@ class Ollama
 
     }
 
+    void setServerURL(const std::string& server_url)
+    {
+        this->server_url = server_url;
+        delete(this->cli);        
+        this->cli = new httplib::Client(server_url);
+    }
+
     private:
 
     std::string server_url;
@@ -109,7 +148,20 @@ class Ollama
 
 namespace ollama
 {
+    // Use directly from the namespace as a singleton
+    static Ollama ollama;
+    
+    inline void setServerURL(const std::string& server_url)
+    {
+        ollama.setServerURL(server_url);
+    }
+
+    inline std::string generate(const std::string& model,const std::string& prompt, bool return_as_json=false)
+    {
+        return ollama.generate(model, prompt, return_as_json);
+    }
 
 }
+
 
 #endif
